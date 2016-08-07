5ecccfb73c29bebc1c490be54cd82282f13f8def
torihutto82 <torihutto82@gmail.com>
2016-08-07 16:17:48 -0400

    created project

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Calendar Page</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="calendar-week">
+			<div class="calendar-week-title">
+				<h1>This Week</h1>
+			</div>
+
+			<ul class="calendar-week-list">
+				
+				<li class="calendar-week-list-day">
+					<h4>Sunday</h4>
+				</li>
+				<li class="calendar-week-list-day">
+					<h4>Monday</h4>
+				</li>
+				<li class="calendar-week-list-day">
+					<h4>Tuesday</h4>
+				</li>
+				<li class="calendar-week-list-day">
+					<h4>Wednesday</h4>
+				</li>
+				<li class="calendar-week-list-day">
+					<h4>Thursday</h4>
+				</li>
+				<li class="calendar-week-list-day">
+					<h4>Friday</h4>
+				</li>
+				<li class="calendar-week-list-day">
+					<h4>Saturday</h4>
+				</li>
+			</ul>
+		</div>
+	</body>
+</html>
